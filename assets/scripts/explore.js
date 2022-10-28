e818c118c00d4454bcb9b87a9b0ff5560d071df2
AungKyaw321 <93343553+AungKyaw321@users.noreply.github.com>
2022-10-28 13:11:35 -0800

    Finished Explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,49 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-
 function init() {
+    var utterance;
+    const getText = document.getElementById("text-to-speak");
+    const currVoice = document.getElementById("voice-select");
+    const getSelectedVoice = document.querySelector("select");
+    const synth = window.speechSynthesis;
+    var textToSpeak;
+    getText.addEventListener("input", (event) => {
+      console.log(event.target.value);
+      textToSpeak = event.target.value;
+    })
+   let voices = [];
+
+    function populateVoiceList() {
+        voices = synth.getVoices();
   
+        for (let i = 0; i < voices.length ; i++) {
+        const option = document.createElement('option');
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+        option.value = i;
+        if (voices[i].default) {
+            option.textContent += ' — DEFAULT';
+        }
+  
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        currVoice.appendChild(option);
+        }
+    }
+    populateVoiceList();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+    const button = document.querySelector('button');
+    button.addEventListener('click', event => {
+        utterance = new SpeechSynthesisUtterance(textToSpeak);
+        utterance.voice = voices[getSelectedVoice.value];
+        synth.speak(utterance);
+        utterance.onstart = event => {
+            document.querySelector("#explore img").src = "assets/images/smiling-open.png";
+           }
+        utterance.addEventListener('end', event => {
+            document.querySelector("#explore img").src = "assets/images/smiling.png";
+           })
+    })
 }
